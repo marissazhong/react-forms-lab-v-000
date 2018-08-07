38abf83cfb776548658d67d06a7fa89ca8774816
Marissa Zhong <mwz2101@columbia.edu>
2018-08-07 13:25:43 +0000

    automatically backed up by learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,7 +18,8 @@ class LoginForm extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault()
-    if ( this.state.username === '' || this.state.password === '') return (this.props.onSubmit(this.state))
+    if ( this.state.username === '' || this.state.password === '') return
+    this.props.onSubmit(this.state)
   }
 
   render() {
