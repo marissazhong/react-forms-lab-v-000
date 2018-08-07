0b9b7f679c47521fca9d1f1ba6ae4406668216f3
Marissa Zhong <mwz2101@columbia.edu>
2018-08-07 13:25:53 +0000

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,17 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: ''
+    };
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" onChange={event => this.setState({tweet: event.target.value})} value={this.state.tweet} />
+        {this.props.maxChars-this.state.tweet.length}
       </div>
     );
   }
